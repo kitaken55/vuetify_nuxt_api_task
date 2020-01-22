1c6fb6155ef3e4f6794dad164431efe4399097c4
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2020-01-22 11:43:45 +0900

    first commit getUserまで

---
+++ pages/getUser.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-app>
+    <v-data-table
+        :headers="headers"
+        :items="getUsers"
+        :items-per-page="5"
+        class="elevation-1"
+    ></v-data-table>
+    </v-app>
+</template>
+
+
+<script>
+import axios from "axios"
+  export default {
+    data() {
+      return {
+        getUsers: [],
+        headers: [
+          {
+            text: '名前',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+        ],
+      }
+    },
+    created() {
+        axios.get('https://teachapi.herokuapp.com/users', {
+        headers: {"Authorization": "Bearer JHBgQ17rEdLFFf4Q88LWCAtt" }
+        })
+        .then(response => response.data)
+        .then(object => {
+            this.getUsers = object
+        })
+        .catch(error => {
+            alert(error);
+        });
+    }
+  }
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-card>
+            <v-card-title>
+            <h1 class="display-1">ログイン</h1>
+            </v-card-title>
+            <v-card-text>
+            <v-form>
+                <v-text-field label="ユーザ名" v-model="text" />
+                <p>{{ text }}</p>
+                <v-text-field label="パスワード" />
+            </v-form>
+            </v-card-text>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            text: ""
+        }
+    }
+}
+</script>
